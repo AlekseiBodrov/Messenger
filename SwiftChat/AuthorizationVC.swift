d527136b796b4267ab5ad513c572b161a597669e
Aleksei Bodrov <bellied.strut_0v@icloud.com>
2022-08-26 17:57:18 +0300

    GreetingsVC add backgroundImageView, imageView 1-4

--- SwiftChat/AuthorizationVC.swift
+++ SwiftChat/AuthorizationVC.swift
@@ -1,8 +1,10 @@
 
 import UIKit
 
-final class AuthorizationVC: UIViewController {
-    
+final class AuthorizationVC: UIViewController, UITextFieldDelegate {
+
+    let screenSize = UIScreen.main.bounds
+
     private lazy var getStartedButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
@@ -11,14 +13,35 @@ final class AuthorizationVC: UIViewController {
         return button
     }()
 
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.frame = CGRect(x: 24, y: 200, width: screenSize.width - 24 - 24, height: 60)
+        textField.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+        textField.delegate = self
+        return textField
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-
-        [getStartedButton].forEach {
+        view.backgroundColor = .black
+        [getStartedButton,
+         textField].forEach {
             view.addSubview($0)
         }
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        textField.endEditing(true)
     }
 
     @objc private func goBack() {
--- SwiftChat/GreetingsVC.swift
+++ SwiftChat/GreetingsVC.swift
@@ -8,9 +8,15 @@ final class GreetingsVC: UIViewController {
     private lazy var backgroundImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.frame = self.view.frame
-        imageView.image = UIImage(named: "")
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .black
+        imageView.image = UIImage(named: "backgroundImage")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    private lazy var backgroundFiltr: UIImageView = {
+        let imageView = UIImageView()
+        imageView.frame = self.view.frame
+        imageView.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.75)
         return imageView
     }()
 
@@ -23,11 +29,33 @@ final class GreetingsVC: UIViewController {
         return view
     }()
 
+    private lazy var titleLable: UILabel = {
+        let lable = UILabel()
+        lable.frame = CGRect(x: 0, y: 60, width: screenSize.width, height: 60)
+        lable.font = .systemFont(ofSize: 24, weight: .bold)
+        lable.text = "Enjoy the new experience of\nchatting with global friends"
+        lable.textAlignment = .center
+        lable.numberOfLines = 2
+        return lable
+    }()
+
+    private lazy var subtitleLable: UILabel = {
+        let lable = UILabel()
+        lable.frame = CGRect(x: 0, y: 130, width: screenSize.width, height: 20)
+        lable.font = .systemFont(ofSize: 16, weight: .medium)
+        lable.text = "  Connect people arround the world for free"
+        lable.textColor = .darkGray
+        lable.alpha = 0.7
+        lable.textAlignment = .center
+        return lable
+    }()
+
     private lazy var getStartedButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 47, y: 200, width: screenSize.width - 47 - 47, height: 67)
         button.setTitle("Get Started", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        button.addTarget(self, action: #selector(touchDown) , for: .touchDown)
         button.addTarget(self, action: #selector(showAuth), for: .touchUpInside)
 
         button.setBackgroundColor(.purple, for: .normal)
@@ -39,38 +67,55 @@ final class GreetingsVC: UIViewController {
         button.layer.shadowRadius = 20
         button.layer.cornerRadius = 33
         button.layer.masksToBounds = true
+
         return button
+    }()
+
+    private lazy var containerImageView : UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 27, y: 117,
+                            width: screenSize.width - 27 - 27,
+                            height: screenSize.width - 27 - 27)
+        return view
     }()
 
     private lazy var imageView1: UIImageView = {
         let imageView = UIImageView()
-        imageView.frame = CGRect(x: 0 , y: screenSize.height / 3 , width: 150, height: 150)
-        imageView.backgroundColor = .red
-        imageView.image = UIImage(named: "")
+        imageView.frame = CGRect(x: 3 ,
+                                 y: 3 ,
+                                 width: containerImageView.bounds.width / 2 - 6,
+                                 height: containerImageView.bounds.height / 2 - 6)
+        imageView.image = UIImage(named: "imageView1")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     private lazy var imageView2: UIImageView = {
         let imageView = UIImageView()
-        imageView.frame = CGRect(x: screenSize.width / 2, y: screenSize.height / 3 , width: 150, height: 150)
-        imageView.backgroundColor = .orange
-        imageView.image = UIImage(named: "")
+        imageView.frame = CGRect(x: containerImageView.bounds.width / 2 + 3,
+                                 y: 3 ,
+                                 width: containerImageView.bounds.width / 2 - 6,
+                                 height: containerImageView.bounds.width / 2 - 6)
+        imageView.image = UIImage(named: "imageView2")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     private lazy var imageView3: UIImageView = {
         let imageView = UIImageView()
-        imageView.frame = CGRect(x: 0, y: screenSize.height * 2 / 3 , width: 150, height: 150)
-        imageView.backgroundColor = .yellow
-        imageView.image = UIImage(named: "")
+        imageView.frame = CGRect(x: 3,
+                                 y: containerImageView.bounds.width / 2 + 3,
+                                 width: containerImageView.bounds.width / 2 - 6,
+                                 height: containerImageView.bounds.width / 2 - 6)
+        imageView.image = UIImage(named: "imageView3")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     private lazy var imageView4: UIImageView = {
         let imageView = UIImageView()
-        imageView.frame = CGRect(x: screenSize.width / 2, y: screenSize.height * 2 / 3 , width: 150, height: 150)
-        imageView.backgroundColor = .blue
-        imageView.image = UIImage(named: "")
+        imageView.frame = CGRect(x: containerImageView.bounds.width / 2 + 3,
+                                 y: containerImageView.bounds.width / 2 + 3,
+                                 width: containerImageView.bounds.width / 2 - 6,
+                                 height: containerImageView.bounds.width / 2 - 6)
+        imageView.image = UIImage(named: "imageView4")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
@@ -79,24 +124,41 @@ final class GreetingsVC: UIViewController {
         super.viewDidLoad()
 
         // backgroundView +
-        // 4x UIImageView
-        // UIView - containeer
-        // titleLable numberOfLines = 2
-        // subtitleLable
-        // getStartedButton
+        // 4x UIImageView +
+        // UIView - containeer +
+        // titleLable numberOfLines = 2 +
+        // subtitleLable +
+        // getStartedButton +
         // UIImageView
 
-        [backgroundImageView,
-         containerView,
-         imageView1,
+        // анимация выскакивает containerView
+        // анимация перед переходом на новый экран
+
+        [getStartedButton,
+         titleLable,
+         subtitleLable].forEach {
+                containerView.addSubview($0)
+        }
+
+        [imageView1,
          imageView2,
          imageView3,
          imageView4].forEach {
-            view.addSubview($0)
+            containerImageView.addSubview($0)
         }
 
-        [getStartedButton].forEach {
-                containerView.addSubview($0)
+        [backgroundImageView,
+         backgroundFiltr,
+         containerImageView,
+         containerView].forEach {
+            view.addSubview($0)
+        }
+    }
+
+    @objc func touchDown() {
+        UIView.animate(withDuration: 0.2) {
+            self.getStartedButton.transform = CGAffineTransform.init(scaleX: 0.98, y: 0.97)
+        } completion: { _ in
         }
     }
 
@@ -111,6 +173,10 @@ final class GreetingsVC: UIViewController {
 
         present(authorizationVC, animated: true) {
             print("presented AuthorizationVC")
+        }
+        UIView.animate(withDuration: 0.1) {
+            self.getStartedButton.transform = .identity
+        } completion: { _ in
         }
     }
 
